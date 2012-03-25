a5ec54a6d042121366a2b88b630615b7018a14c4
Clust3r <ghys.jeremy@gmail.com>
2012-03-25 07:14:14 +0200

    update

--- src/com/clustycontrol/fragments/Lockscreens.java
+++ src/com/clustycontrol/fragments/Lockscreens.java
@@ -74,6 +74,9 @@ public class Lockscreens extends SettingsPreferenceFragment implements
     private static final String PREF_LOCKSCREEN_CALENDAR = "enable_calendar";
     private static final String PREF_LOCKSCREEN_CALENDAR_FLIP = "lockscreen_calendar_flip";
     private static final String PREF_LOCKSCREEN_CALENDAR_SOURCES = "lockscreen_calendar_sources";
+	private static final String PREF_LOCKSCREEN_CALENDAR_RANGE = "lockscreen_calendar_range";
+	private static final String PREF_LOCKSCREEN_CALENDAR_HIDE_ONGOING = "lockscreen_calendar_hide_ongoing";
+	private static final String PREF_LOCKSCREEN_CALENDAR_USE_COLORS = "lockscreen_calendar_use_colors";
     private static final String PREF_LOCKSCREEN_CALENDAR_INTERVAL = "lockscreen_calendar_interval";
 
     private static final String PREF_SHOW_LOCK_BEFORE_UNLOCK = "show_lock_before_unlock";
@@ -100,6 +103,9 @@ public class Lockscreens extends SettingsPreferenceFragment implements
     CheckBoxPreference mLockscreenCalendarFlip;
     Preference mCalendarSources;
     ListPreference mCalendarInterval;
+	ListPreference mCalendarRange;
+	CheckBoxPreference mLockscreenCalendarHideOngoing;
+	CheckBoxPreference mLockscreenCalendarUseColors;
 
     Preference mLockscreenWallpaper;
 
@@ -169,6 +175,14 @@ public class Lockscreens extends SettingsPreferenceFragment implements
         mLockscreenCalendarFlip = (CheckBoxPreference) findPreference(PREF_LOCKSCREEN_CALENDAR_FLIP);
         mLockscreenCalendarFlip.setChecked(Settings.System.getInt(getActivity()
                 .getContentResolver(), Settings.System.LOCKSCREEN_CALENDAR_FLIP, 0) == 1);
+				
+		mLockscreenCalendarHideOngoing = (CheckBoxPreference) findPreference(PREF_LOCKSCREEN_CALENDAR_HIDE_ONGOING);
+        mLockscreenCalendarHideOngoing.setChecked(Settings.System.getInt(getActivity()
+                .getContentResolver(), Settings.System.LOCKSCREEN_CALENDAR_HIDE_ONGOING, 0) == 1);
+        
+        mLockscreenCalendarUseColors = (CheckBoxPreference) findPreference(PREF_LOCKSCREEN_CALENDAR_USE_COLORS);
+        mLockscreenCalendarUseColors.setChecked(Settings.System.getInt(getActivity()
+                .getContentResolver(), Settings.System.LOCKSCREEN_CALENDAR_USE_COLORS, 0) == 1);
 
         mCalendarSources = findPreference(PREF_LOCKSCREEN_CALENDAR_SOURCES);
 
@@ -176,6 +190,11 @@ public class Lockscreens extends SettingsPreferenceFragment implements
         mCalendarInterval.setOnPreferenceChangeListener(this);
         mCalendarInterval.setValue(Settings.System.getInt(getActivity().getContentResolver(),
                 Settings.System.LOCKSCREEN_CALENDAR_INTERVAL, 2500) + "");
+
+		mCalendarRange = (ListPreference) findPreference(PREF_LOCKSCREEN_CALENDAR_RANGE);
+        mCalendarRange.setOnPreferenceChangeListener(this);
+        mCalendarRange.setValue(Settings.System.getLong(getActivity().getContentResolver(),
+                Settings.System.LOCKSCREEN_CALENDAR_RANGE, 86400000) + "");
 
         mPicker = new ShortcutPickerHelper(this, this);
 
@@ -302,6 +321,20 @@ public class Lockscreens extends SettingsPreferenceFragment implements
 
             Settings.System.putInt(getActivity().getContentResolver(),
                     Settings.System.LOCKSCREEN_CALENDAR_FLIP,
+                    ((CheckBoxPreference) preference).isChecked() ? 1 : 0);
+            return true;
+		
+		} else if (preference == mLockscreenCalendarHideOngoing) {
+			
+			Settings.System.putInt(getActivity().getContentResolver(),
+                    Settings.System.LOCKSCREEN_CALENDAR_HIDE_ONGOING,
+                    ((CheckBoxPreference) preference).isChecked() ? 1 : 0);
+            return true;
+
+        } else if (preference == mLockscreenCalendarUseColors) {
+
+            Settings.System.putInt(getActivity().getContentResolver(),
+                    Settings.System.LOCKSCREEN_CALENDAR_USE_COLORS,
                     ((CheckBoxPreference) preference).isChecked() ? 1 : 0);
             return true;
 
@@ -611,7 +644,12 @@ public class Lockscreens extends SettingsPreferenceFragment implements
             Settings.System.putInt(getActivity().getContentResolver(),
                     Settings.System.LOCKSCREEN_CALENDAR_INTERVAL, val);
             return true;
-
+		
+		} else if (preference == mCalendarRange) {
+			long val = Long.parseLong((String) newValue);
+            Settings.System.putLong(getActivity().getContentResolver(),
+                    Settings.System.LOCKSCREEN_CALENDAR_RANGE, val);
+            return true;
         } else if (preference == mLockscreenTextColor) {
             String hex = ColorPickerPreference.convertToARGB(Integer.valueOf(String
                     .valueOf(newValue)));
